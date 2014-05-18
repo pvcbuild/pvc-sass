4890d58270dc97ecaa082f170caff8af944c4b74
Brandon Stirnaman <stirno@gmail.com>
2014-05-17 20:24:12 -0500

    Add PvcSass plugin

---
+++ src/PvcSass.cs
@@ -0,0 +1,29 @@
+﻿using PvcCore;
+using SassAndCoffee.Ruby.Sass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvcSass
+{
+    public class PvcSass : PvcPlugin
+    {
+        public override IEnumerable<PvcCore.PvcStream> Execute(IEnumerable<PvcCore.PvcStream> inputStreams)
+        {
+            var resultStreams = new List<PvcStream>();
+
+            foreach (var inputStream in inputStreams)
+            {
+                var sassContent = PvcUtil.StreamToTempFile(inputStream);
+                var cssContent = new SassCompiler().Compile(sassContent, false, null);
+
+                var resultStream = PvcUtil.StringToStream(cssContent, inputStream.StreamName);
+                resultStreams.Add(resultStream);
+            }
+
+            return resultStreams;
+        }
+    }
+}
